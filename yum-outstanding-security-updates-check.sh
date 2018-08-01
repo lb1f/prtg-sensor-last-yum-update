d47e22797a90b56407dbd30870d87e39c7449b53
lb1f <41776771+lb1f@users.noreply.github.com>
2018-08-01 08:06:32 +0100

    Update yum-outstanding-security-updates-check.sh
    Script works, takes about 20-30 seconds depending on server speed and internet connection.
    
    Plan to add some redirect and a check so that it will only check live every hour or two, output date and output result to a file in /tmp and check that before running the command again and updating this file.

--- yum-outstanding-security-updates-check.sh
+++ yum-outstanding-security-updates-check.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
 
+#returncode:value:message
+#0      OK
+#1      WARNING
+#2      System Error (e.g. a network/socket error)
+#3      Protocol Error (e.g. web server returns a 404)
+#4      Content Error (e.g. a web page does not contain a required word)
+
+SUPDATES=`yum check-update --security | grep "needed for" | cut -d " " -f1`
+#grep U matches with Update and U
+#head -n1 takes upper line
+#cut sets delimiter on spaces and takes first column
+#OUTPUT EXAMPLE 2
+
+echo "0:$SUPDATES:$SUPDATES Outstanding Security Updates"
